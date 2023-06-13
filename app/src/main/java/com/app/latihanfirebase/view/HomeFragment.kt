9e1dcf0492d2be59e3b452400787daa40ff8a1d6
Raynaldi Zulfikar <raynaldizoel@gmail.com>
2023-06-14 04:47:47 +0700

    Initial commit

---
+++ app/src/main/java/com/app/latihanfirebase/view/HomeFragment.kt
@@ -0,0 +1,50 @@
+package com.app.latihanfirebase.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.app.latihanfirebase.adapter.HomeAdapter
+import com.app.latihanfirebase.databinding.FragmentHomeBinding
+import com.app.latihanfirebase.model.Mahasiswa
+import com.google.firebase.database.*
+
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var adapter: HomeAdapter
+    private lateinit var databaseRef: DatabaseReference
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        adapter = HomeAdapter(ArrayList())
+        binding.rvUser.adapter = adapter
+        binding.rvUser.layoutManager = LinearLayoutManager(requireContext())
+
+        databaseRef = FirebaseDatabase.getInstance().getReference("mahasiswa")
+        databaseRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val mahasiswaList = ArrayList<Mahasiswa>()
+                for (snapshot in dataSnapshot.children) {
+                    val mahasiswa = snapshot.getValue(Mahasiswa::class.java)
+                    mahasiswa?.let { mahasiswaList.add(it) }
+                }
+                adapter.setData(mahasiswaList)
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Handle database error
+            }
+        })
+    }
+}
